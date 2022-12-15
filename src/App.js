0eef64e96ff52345720df02ef1d7f03e6ecf0ef0
Kalvium <program@kalvieducation.com>
2022-12-15 12:21:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import GallaryHeader from "./components/GallaryHeader";
+import GallaryBody from "./components/GallaryBody";
+import GallaryFooter from "./components/GallaryFooter";
 
-function App() {
+function App(props) {
+  const imageData = props.data;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GallaryHeader/>
+      <GallaryBody imageData={imageData}/>
+      <GallaryFooter/>
     </div>
-  );
+  )
 }
 
 export default App;
